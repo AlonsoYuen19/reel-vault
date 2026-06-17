920fccdc19d513d0a88161ff36be3240a78a67c5
AlonsoYuen <wwepg15@gmail.com>
2026-06-17 14:28:02 -0500

    feature/navigation
    commit 9ccd07a36af841279cad7b743b8abd58e1c9aeba
    Author: AlonsoYuen <wwepg15@gmail.com>
    Date:   Wed Jun 17 00:45:17 2026 -0500
    
        feat(navigation): integrate go_router with stateful shell bottom navigation

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:reel_vault/features/home/presentation/screens/home_screen.dart';
+import 'package:reel_vault/features/navigation/presentation/screens/main_shell.dart';
+
+final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
+
+final GoRouter appRouter = GoRouter(
+  navigatorKey: _rootNavigatorKey,
+  initialLocation: '/',
+
+  routes: <RouteBase>[
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) {
+        return MainShell(
+          navigationShell: navigationShell,
+        );
+      },
+      branches: <StatefulShellBranch>[
+        StatefulShellBranch(
+          routes: <RouteBase>[
+            GoRoute(
+              path: '/',
+              builder: (context, state) => const HomeScreen(),
+            ),
+          ],
+        ),
+        // Rama 2: Buscar (Placeholder)
+        StatefulShellBranch(
+          routes: <RouteBase>[
+            GoRoute(
+              path: '/search',
+              builder: (context, state) => const Scaffold(
+                body: Center(
+                  child: Text('Buscar (Placeholder)'),
+                ),
+              ),
+            ),
+          ],
+        ),
+        // Rama 3: Favoritos (Placeholder)
+        StatefulShellBranch(
+          routes: <RouteBase>[
+            GoRoute(
+              path: '/favorites',
+              builder: (context, state) => const Scaffold(
+                body: Center(
+                  child: Text('Favoritos (Placeholder)'),
+                ),
+              ),
+            ),
+          ],
+        ),
+        // Rama 4: Perfil (Placeholder)
+        StatefulShellBranch(
+          routes: <RouteBase>[
+            GoRoute(
+              path: '/profile',
+              builder: (context, state) => const Scaffold(
+                body: Center(
+                  child: Text('Perfil (Placeholder)'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
+  ],
+);
