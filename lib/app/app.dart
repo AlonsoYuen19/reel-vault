9ccd07a36af841279cad7b743b8abd58e1c9aeba
AlonsoYuen <wwepg15@gmail.com>
2026-06-17 00:45:17 -0500

    feat(navigation): integrate go_router with stateful shell bottom navigation

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:reel_vault/core/router/app_router.dart';
 import 'package:reel_vault/core/theme/app_theme.dart';
-import 'package:reel_vault/features/home/presentation/screens/home_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       theme: AppTheme.darkTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.dark,
-      home: const HomeScreen(),
+      routerConfig: appRouter,
     );
   }
 }
